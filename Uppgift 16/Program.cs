f4866e96a0c6598ce58ea1fd62b6ceb5af064df2
Admin <Admin@TEB123001>
2022-10-05 12:01:06 +0200

    Add project files.

---
+++ Uppgift 16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_3._16
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många minuter är låten");
+            int minuter  = int.Parse(Console.ReadLine());
+            Console.WriteLine("hur många sekunder är låten?");
+            int sekunder = int.Parse(Console.ReadLine());
+            int totalasekunder =minuter * 60 + sekunder;
+            int mingräns = 2 * 60 + 45;
+            int maxgräns = 4 * 60 + 20;
+
+            if ( totalasekunder >= mingräns && totalasekunder <= maxgräns)
+            {
+                Console.WriteLine("det ska spelas låten");    
+            }
+            else
+            {
+                Console.WriteLine("låten får inte spelas");
+            }
+    }
+}
